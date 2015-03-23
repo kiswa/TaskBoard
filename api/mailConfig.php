f8493f80a3d9f05d4934b366c426abce48d72cb5
Danila Balabaev <shattenjagger@yandex.ru>
2015-03-23 22:43:24 +0300

    Minor change

--- api/mailConfig.php
+++ api/mailConfig.php
@@ -6,4 +6,4 @@ $MAIL_PASSWORD = 'secretpassword';                           // SMTP password
 $MAIL_SMTPSECURE = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $MAIL_PORT = 465;                                    // TCP port to connect to
 $MAIL_FROM = 'user@somehost.com';
-$MAIL_FROMNAME = 'Taskboard';
+$MAIL_FROMNAME = 'TaskBoard';
