c0c5c8179e29e2b9c6efb0eada1aa10e3808fda7
Martin Holzhauer <martin@holzhauer.eu>
2015-04-19 13:57:05 +0200

    added a simple routing script for the php internal webserver to simplify the setup of a development environment

--- api/api.php
+++ api/api.php
@@ -28,7 +28,7 @@ function exceptionHandler($exception) {
 };
 set_exception_handler('exceptionHandler');
 
-R::setup('sqlite:taskboard.db');
+R::setup('sqlite:'.__DIR__.'/taskboard.db');
 createInitialUser();
 
 $app->notFound(function() use ($app, $jsonResponse) {
