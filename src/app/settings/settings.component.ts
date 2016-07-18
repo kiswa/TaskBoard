12d8b2b1cbfcdb757be13a20dd41c84e486f8421
Matthew Ross <kiswa.com@gmail.com>
2016-07-17 21:19:38 -0400

    Start separating settings sections into components

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
 import { TopNav } from '../shared/index';
+import { UserSettings } from './user-settings/user-settings.component';
 
 @Component({
     selector: 'tb-settings',
     templateUrl: 'app/settings/settings.component.html',
-    directives: [ TopNav ]
+    directives: [ TopNav, UserSettings ]
 })
 export class Settings {
 }
---
+++ src/app/settings/user-settings/user-settings.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'tb-user-settings',
+    templateUrl: 'app/settings/user-settings/user-settings.component.html'
+})
+export class UserSettings {
+}
+
