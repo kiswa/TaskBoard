8ab625753ea151a00b53b513891ab49206c7074b
kiswa <kiswa.com@gmail.com>
2016-07-26 21:19:52 +0000

    Change User model to class

--- src/app/shared/models/user.model.ts
+++ src/app/shared/models/user.model.ts
@@ -1,10 +1,11 @@
-export interface User {
-    default_board_id: number;
-    email: string;
-    id: number;
-    last_login: Date;
-    security_level: number;
-    user_option_id: number;
-    username: string;
+export class User {
+    constructor(public default_board_id: number = 0,
+            public email: string = '',
+            public id: number = 0,
+            public last_login: Date = null,
+            public security_level: number = 3,
+            public user_option_id: number = 0,
+            public username: string = '') {
+    }
 }
 
