e6186aabf0bf1f9f7e8049a93ddc907ef7c02284
kiswa <kiswa.com@gmail.com>
2016-08-25 10:33:29 +0000

    Inline edit component

---
+++ src/app/shared/inline-edit/inline-edit.component.ts
@@ -0,0 +1,32 @@
+import {
+    Component,
+    Input,
+    Output,
+    EventEmitter
+} from '@angular/core';
+
+@Component({
+    selector: 'inline-edit',
+    templateUrl: 'app/shared/inline-edit/inline-edit.component.html'
+})
+export class InlineEdit {
+    private isDisplay = true;
+
+    @Input() text: string;
+    @Output() edit = new EventEmitter<string>();
+
+    beginEdit(el: HTMLElement): void {
+        this.isDisplay = false;
+
+        setTimeout(() => {
+            el.focus();
+        }, 100);
+    }
+
+    editDone(newText: string): void {
+        this.isDisplay = true;
+        this.text = newText;
+        this.edit.emit(this.text);
+    }
+}
+
