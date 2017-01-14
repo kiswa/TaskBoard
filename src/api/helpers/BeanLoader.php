3d0015d41f80aeee53892845071a84aa7498e138
Matthew Ross <kiswa.com@gmail.com>
2017-01-14 16:36:59 -0500

    Refactor BeanLoader

--- src/api/helpers/BeanLoader.php
+++ src/api/helpers/BeanLoader.php
@@ -5,116 +5,61 @@ class BeanLoader {
 
     public static function LoadAttachment(&$attachment, $json) {
         $data = json_decode($json);
-        $loaded = true;
 
-        if (isset($data->filename)) {
-            $attachment->filename = $data->filename;
-        } else {
-            $loaded = false;
-        }
-
-        if (isset($data->name)) {
-            $attachment->name = $data->name;
-        } else {
-            $loaded = false;
-        }
-
-        if (isset($data->type)) {
-            $attachment->type = $data->type;
-        } else {
-            $loaded = false;
-        }
-
-        if (isset($data->user_id)) {
-            $attachment->user_id = $data->user_id;
-        } else {
-            $loaded = false;
-        }
-
+        $attachment->filename = isset($data->filename) ? $data->filename : '';
+        $attachment->name = isset($data->name) ? $data->name : '';
+        $attachment->type = isset($data->type) ? $data->type : '';
+        $attachment->user_id = isset($data->user_id) ? $data->user_id : '';
         $attachment->timestamp = time();
+        $attachment->task_id = isset($data->task_id) ? $data->task_id : '';
 
-        if (isset($data->task_id)) {
-            $attachment->task_id = $data->task_id;
-        } else {
-            $loaded = false;
+        if (!isset($data->filename) || !isset($data->name) ||
+            !isset($data->type) || !isset($data->user_id) ||
+            !isset($data->task_id)) {
+            return false;
         }
 
-        return $loaded;
+        return true;
     }
 
     public static function LoadAutoAction(&$action, $json) {
         $data = json_decode($json);
-        $loaded = true;
 
-        if (isset($data->trigger)) {
-            $action->trigger = $data->trigger;
-        } else {
-            $loaded = false;
+        $action->trigger = isset($data->trigger) ? $data->trigger : '';
+        $action->source_id = isset($data->source_id) ? $data->source_id: '';
+        $action->type = isset($data->type) ? $data->type : '';
+        $action->change_to = isset($data->change_to) ? $data->change_to: '';
+        $action->board_id = isset($data->board_id) ? $data->board_id: '';
+
+        if (!isset($data->trigger) || !isset($data->source_id) ||
+            !isset($data->type) || !isset($data->change_to) ||
+            !isset($data->board_id)) {
+            return false;
         }
 
-        if (isset($data->source_id)) {
-            $action->source_id = $data->source_id;
-        } else {
-            $loaded = false;
-        }
-
-        if (isset($data->type)) {
-            $action->type = $data->type;
-        } else {
-            $loaded = false;
-        }
-
-        if (isset($data->change_to)) {
-            $action->change_to = $data->change_to;
-        } else {
-            $loaded = false;
-        }
-
-        if (isset($data->board_id)) {
-            $action->board_id = $data->board_id;
-        } else {
-            $loaded = false;
-        }
-
-        return $loaded;
+        return true;
     }
 
     public static function LoadBoard(&$board, $json) {
         $data = json_decode($json);
-        $loaded = true;
 
-        if (isset($data->name)) {
-            $board->name = $data->name;
-        } else {
-            $loaded = false;
-        }
-
-        if (isset($data->is_active)) {
-            $board->is_active = $data->is_active;
-        } else {
-            $loaded = false;
-        }
+        $board->name = isset($data->name) ? $data->name : '';
+        $board->is_active = isset($data->is_active) ? $data->is_active : '';
 
         if (isset($data->categories)) {
             self::updateBoardList('category', 'LoadCategory',
-                                  $board->xownCategoryList, $data->categories);
-        } else {
-            $loaded = false;
+                $board->xownCategoryList, $data->categories);
         }
 
         if (isset($data->columns)) {
             self::updateBoardList('column', 'LoadColumn',
-                                  $board->xownColumnList, $data->columns);
-        } else {
-            $loaded = false;
+                $board->xownColumnList, $data->columns);
         }
 
         if (isset($data->issue_trackers)) {
             self::updateBoardList('issuetracker', 'LoadIssueTracker',
-                                  $board->xownIssueTrackerList,
-                                  $data->issue_trackers);
-        } else {
-            $loaded = false;
+                $board->xownIssueTrackerList,
+                $data->issue_trackers);
         }
 
         // Users do not get deleted when removed from a board
@@ -130,223 +75,133 @@ class BeanLoader {
             }
         }
 
-        return $loaded;
+        if (!isset($data->name) || !isset($data->is_active) ||
+            !isset($data->categories) || !isset($data->columns) ||
+            !isset($data->issue_trackers) || !isset($data->users)) {
+            return false;
+        }
+
+        return true;
     }
 
     public static function LoadCategory(&$category, $json) {
         $data = json_decode($json);
-        $loaded = true;
 
-        if (isset($data->name)) {
-            $category->name = $data->name;
-        } else {
-            $loaded = false;
+        $category->name = isset($data->name) ? $data->name : '';
+        $category->default_task_color = isset($data->default_task_color)
+            ? $data->default_task_color : '';
+        $category->board_id = isset($data->board_id) ? $data->board_id : '';
+
+        if (!isset($data->name) || !isset($data->default_task_color) ||
+            !isset($data->board_id)) {
+            return false;
         }
 
-        if (isset($data->default_task_color)) {
-            $category->default_task_color = $data->default_task_color;
-        } else {
-            $loaded = false;
-        }
-
-        if (isset($data->board_id)) {
-            $category->board_id = $data->board_id;
-        } else {
-            $loaded = false;
-        }
-
-        return $loaded;
+        return true;
     }
 
     public static function LoadColumn(&$column, $json) {
         $data = json_decode($json);
-        $loaded = true;
 
-        if (isset($data->name)) {
-            $column->name = $data->name;
-        } else {
-            $loaded = false;
+        $column->name = isset($data->name) ? $data->name : '';
+        $column->position = isset($data->position) ? $data->position : '';
+        $column->board_id = isset($data->board_id) ? $data->board_id : '';
+
+        if (!isset($data->name) || !isset($data->position) ||
+            !isset($data->board_id)) {
+            return false;
         }
 
-        if (isset($data->position)) {
-            $column->position = $data->position;
-        } else {
-            $loaded = false;
-        }
-
-        if (isset($data->board_id)) {
-            $column->board_id = $data->board_id;
-        } else {
-            $loaded = false;
-        }
-
-        return $loaded;
+        return true;
     }
 
     public static function LoadComment(&$comment, $json) {
         $data = json_decode($json);
-        $loaded = true;
 
-        if (isset($data->text)) {
-            $comment->text = $data->text;
-        } else {
-            $loaded = false;
+        $comment->text = isset($data->text) ? $data->text : '';
+        $comment->user_id = isset($data->user_id) ? $data->user_id : '';
+        $comment->task_id = isset($data->task_id) ? $data->task_id : '';
+
+        if (!isset($data->text) || !isset($data->user_id) ||
+            !isset($data->task_id)) {
+            return false;
         }
 
-        if (isset($data->user_id)) {
-            $comment->user_id = $data->user_id;
-        } else {
-            $loaded = false;
-        }
-
-        if (isset($data->task_id)) {
-            $comment->task_id = $data->task_id;
-        } else {
-            $loaded = false;
-        }
-
-        return $loaded;
+        return true;
     }
 
     public static function LoadIssueTracker(&$tracker, $json) {
         $data = json_decode($json);
-        $loaded = true;
 
-        if (isset($data->url)) {
-            $tracker->url = $data->url;
-        } else {
-            $loaded = false;
+        $tracker->url = isset($data->url) ? $data->url : '';
+        $tracker->regex = isset($data->regex) ? $data->regex : '';
+        $tracker->board_id = isset($data->board_id) ? $data->board_id : '';
+
+        if (!isset($data->url) || !isset($data->regex) ||
+            !isset($data->board_id)) {
+            return false;
         }
 
-        if (isset($data->regex)){
-            $tracker->regex = $data->regex;
-        } else {
-            $loaded = false;
-        }
-
-        if (isset($data->board_id)) {
-            $tracker->board_id = $data->board_id;
-        } else {
-            $loaded = false;
-        }
-
-        return $loaded;
+        return true;
     }
 
     public static function LoadTask(&$task, $json) {
         $data = json_decode($json);
-        $loaded = true;
 
-        if (isset($data->title)) {
-            $task->title = $data->title;
-        } else {
-            $loaded = false;
+        $task->title = isset($data->title) ? $data->title : '';
+        $task->description = isset($data->description)
+            ? $data->description : '';
+        $task->assignee = isset($data->assignee) ? $data->assignee : '';
+        $task->category_id = isset($data->category_id)
+            ? $data->category_id : '';
+        $task->color = isset($data->color) ? $data->color : '';
+        $task->due_date = isset($data->due_date) ? $data->due_date : '';
+        $task->points = isset($data->points) ? $data->points : '';
+        $task->position = isset($data->position) ? $data->position : '';
+        $task->column_id = isset($data->column_id) ? $data->column_id : '';
+
+        if (!isset($data->title) || !isset($data->description) ||
+            !isset($data->position) || !isset($data->column_id)) {
+            return false;
         }
 
-        if (isset($data->description)) {
-            $task->description = $data->description;
-        }
-
-        if (isset($data->assignee)) {
-            $task->assignee = $data->assignee;
-        }
-
-        if (isset($data->category_id)) {
-            $task->category_id = $data->category_id;
-        }
-
-        if (isset($data->color)) {
-            $task->color = $data->color;
-        }
-
-        if (isset($data->due_date)) {
-            $task->due_date = $data->due_date;
-        }
-
-        if (isset($data->points)) {
-            $task->points = $data->points;
-        }
-
-        if (isset($data->position)) {
-            $task->position = $data->position;
-        } else {
-            $loaded = false;
-        }
-
-        if (isset($data->column_id)) {
-            $task->column_id = $data->column_id;
-        } else {
-            $loaded = false;
-        }
-
-        return $loaded;
+        return true;
     }
 
     public static function LoadUser(&$user, $json) {
         $data = json_decode($json);
-        $loaded = true;
 
-        if (isset($data->security_level)) {
-            $user->security_level = $data->security_level;
-        } else {
-            $loaded = false;
+        $user->security_level = isset($data->security_level)
+            ? $data->security_level : '';
+        $user->username = isset($data->username) ? $data->username : '';
+        $user->email = isset($data->email) ? $data->email : '';
+        $user->default_board_id = isset($data->default_board_id)
+            ? $data->default_board_id : '';
+        $user->user_option_id = isset($data->user_option_id)
+            ? $data->user_option_id : '';
+        $user->last_login = isset($data->last_login) ? $data->last_login : '';
+        $user->password_hash = isset($data->password_hash)
+            ? $data->password_hash : '';
+
+        if (!isset($data->security_level) || !isset($data->username)) {
+            return false;
         }
 
-        if (isset($data->username)) {
-            $user->username = $data->username;
-        } else {
-            $loaded = false;
-        }
-
-        if (isset($data->email)) {
-            $user->email = $data->email;
-        }
-
-        if (isset($data->default_board_id)) {
-            $user->default_board_id = $data->default_board_id;
-        }
-
-        if (isset($data->user_option_id)) {
-            $user->user_option_id = $data->user_option_id;
-        }
-
-        if (isset($data->last_login)) {
-            $user->last_login = $data->last_login;
-        }
-
-        if (isset($data->password_hash)) {
-            $user->password_hash = $data->password_hash;
-        }
-
-        return $loaded;
+        return true;
     }
 
     public static function LoadUserOption(&$opts, $json) {
         $data = json_decode($json);
-        $loaded = true;
 
-        if (isset($data->new_tasks_at_bottom)) {
-            $opts->new_tasks_at_bottom = (boolean)$data->new_tasks_at_bottom;
-        } else {
-            $loaded = false;
-        }
+        $opts->new_tasks_at_bottom = (boolean)$data->new_tasks_at_bottom;
+        $opts->show_animations = (boolean)$data->show_animations;
+        $opts->show_assignee = (boolean)$data->show_assignee;
+        $opts->multiple_tasks_per_row = (boolean)$data->multiple_tasks_per_row;
 
-        if (isset($data->show_animations)) {
-            $opts->show_animations = (boolean)$data->show_animations;
-        } else {
-            $loaded = false;
-        }
-
-        if (isset($data->show_assignee)) {
-            $opts->show_assignee = (boolean)$data->show_assignee;
-        } else {
-            $loaded = false;
-        }
-
-        if (isset($data->multiple_tasks_per_row)) {
-            $opts->multiple_tasks_per_row = (boolean)$data->multiple_tasks_per_row;
-        } else {
-            $loaded = false;
+        if (!isset($data->new_tasks_at_bottom) ||
+            !isset($data->show_animations) || !isset($data->show_assignee) ||
+            !isset($data->multiple_tasks_per_row)) {
+            return false;
         }
 
         return $loaded;
