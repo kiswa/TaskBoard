363d1110b4b380e259e758562e65421600f5e094
kiswa <kiswa.com@gmail.com>
2017-02-03 23:24:14 +0000

    Add missed service file

---
+++ src/app/board/board.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
+import {
+    ApiResponse,
+    User,
+    Board
+} from '../shared/index';
+
+@Injectable()
+export class BoardService {
+    constructor(private http: Http) {
+    }
+
+    getBoards(): Observable<ApiResponse> {
+        return this.http.get('api/boards')
+            .map(res => {
+                let response: ApiResponse = res.json();
+                return response;
+            })
+            .catch((res, caught) => {
+                let response: ApiResponse = res.json();
+                return Observable.of(response);
+            });
+    }
+}
+
